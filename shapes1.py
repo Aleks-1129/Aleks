e61c64edbe5fb6e68a10eb58152d05997378c6bf
Aleks Daniel Corrales Mendivil <aleksdcm@hotmail.com>
2020-10-27 17:28:37 -0700

    modificación_owner

--- shapes1.py
+++ shapes1.py
@@ -1,13 +1,30 @@
 import turtle
 from turtle import *
 from turtle import Screen
-from freegames import vector
     
 def emptySquare():
     pass  # TODO
     
 def filledSquare():
   pass  # TODO
+
+def emptySquare():
+    t = turtle.Turtle()
+    for i in range(4):
+        t.forward(50)
+        t.right(90)
+        # Rotate clockwise by 90 degrees
+def filledSquare():
+    s = int(input("Enter the length of the side of the square: ")) 
+    col = input("Enter the color name or hex value of color(# RRGGBB): ") 
+    t = turtle.Turtle()
+    t.fillcolor(col)
+    t.begin_fill()
+    for i in range(4):
+        t.forward(s)
+        t.right(90)
+# Rotate clockwise by 90 degrees    t.end_fill()
+
   
 while True:
     screen = Screen()
@@ -18,4 +35,8 @@ while True:
         emptySquare()
     elif (answer == '2'):
         filledSquare()
- 
+    elif (answer == '3'):
+        emptySquare()
+    elif (answer == '4'):
+        filledSquare()
+

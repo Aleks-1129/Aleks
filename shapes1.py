05e62caa8fae8be2a630d8462e041d05c99d813e
unknown <miguelpadresm@hotmail.com>
2020-10-27 18:33:51 -0700

    pero sigo siendo el rey

--- shapes1.py
+++ shapes1.py
@@ -3,17 +3,12 @@ from turtle import *
 from turtle import Screen
     
 def emptySquare():
-    pass  # TODO
-    
-def filledSquare():
-  pass  # TODO
-
-def emptySquare():
     t = turtle.Turtle()
+
     for i in range(4):
         t.forward(50)
-        t.right(90)
-        # Rotate clockwise by 90 degrees
+        t.right(90)     # Rotate clockwise by 90 degrees
+    
 def filledSquare():
     s = int(input("Enter the length of the side of the square: ")) 
     col = input("Enter the color name or hex value of color(# RRGGBB): ") 
@@ -22,9 +17,22 @@ def filledSquare():
     t.begin_fill()
     for i in range(4):
         t.forward(s)
-        t.right(90)
-# Rotate clockwise by 90 degrees    t.end_fill()
+        t.right(90)     # Rotate clockwise by 90 degrees
+    t.end_fill()
 
+
+def empyCircle():
+     t = turtle.Turtle()
+     t.circle(100)
+
+
+def filledCircle():
+    t = turtle.Turtle()
+    t.fillcolor('red')
+    t.begin_fill()
+    t.circle(100)
+    t.end_fill()
+    
   
 while True:
     screen = Screen()
@@ -36,7 +44,8 @@ while True:
     elif (answer == '2'):
         filledSquare()
     elif (answer == '3'):
-        emptySquare()
+        emptyCircle()
     elif (answer == '4'):
-        filledSquare()
+        filledCircle()
+ 
 
